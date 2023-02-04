96c65680dc8c2c85596c20e4fb93bb05db3e3eb4
Soroush Rasti <rasti.soroush@gmail.com>
2023-02-05 00:54:40 +0100

    add template for recipes

--- src/app/recepies/recipes-list/recipes-list.component.ts
+++ src/app/recepies/recipes-list/recipes-list.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import {Recipe} from "../recipe.model";
 
 @Component({
   selector: 'app-recipes-list',
   templateUrl: './recipes-list.component.html',
   styleUrls: ['./recipes-list.component.css']
 })
-export class RecipesListComponent {
+export class RecipesListComponent implements OnInit {
+   recipes: Recipe[]=[
+     new Recipe("Test Recipe",
+       "test"
+     ,"https://www.averiecooks.com/wp-content/uploads/2021/01/garlicbutterchicken-5.jpg"
+     ),
+     new Recipe("Test Recipe",
+       "test"
+       ,"https://www.averiecooks.com/wp-content/uploads/2021/01/garlicbutterchicken-5.jpg"
+     )
 
+   ];
+   constructor() {
+   }
+   ngOnInit(){
+   }
 }
