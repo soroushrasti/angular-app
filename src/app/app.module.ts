7d084493ae919c84708d9df547cf2ce7a1f0010a
Soroush Rasti <rasti.soroush@gmail.com>
2023-02-04 23:36:06 +0100

    add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms'
 import { AppComponent } from './app.component';
+import {HeaderComponent} from "./header/header.component";
+import {ShoppingListComponent} from "./shopping-list/shopping-list.component";
+import { RecepiesComponent } from './recepies/recepies.component';
+import { RecipesListComponent } from './recepies/recipes-list/recipes-list.component';
+import { RecipesDetailComponent } from './recepies/recipes-detail/recipes-detail.component';
+import { RecipeItemComponent } from './recepies/recipes-list/recipe-item/recipe-item.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
+    ShoppingListComponent,
+    RecepiesComponent,
+    RecipesListComponent,
+    RecipesDetailComponent,
+    RecipeItemComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,10 @@
+import {Component} from "@angular/core";
+
+@Component({
+  templateUrl: 'header.component.html',
+  selector:'app-header',
+
+})
+export class HeaderComponent{
+
+}
